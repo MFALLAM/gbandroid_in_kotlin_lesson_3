7390915cbaecaaf0e78778885eff3e351c9f76a7
MFALLAM <mohammed.fathi.allam@gmail.com>
2022-10-16 21:47:18 +0300

    View Model is created

---
+++ app/src/main/java/com/example/weatherapp/view/WeatherFragment.kt
@@ -0,0 +1,84 @@
+package com.example.weatherapp.view
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.weatherapp.R
+import com.example.weatherapp.model.Weather
+import com.example.weatherapp.databinding.FragmentWeatherBinding
+import com.example.weatherapp.viewmodel.AppState
+import com.example.weatherapp.viewmodel.WeatherViewModel
+import com.google.android.material.snackbar.Snackbar
+
+
+class WeatherFragment : Fragment() {
+
+    companion object {
+        fun newInstance() : WeatherFragment = WeatherFragment()
+    }
+
+    private lateinit var viewModel : WeatherViewModel
+    private var _binding : FragmentWeatherBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWeatherBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
+        val observer = Observer<AppState> { renderData(it) }
+        viewModel.getLiveData().observe(viewLifecycleOwner, observer)
+        viewModel.getWeatherFromLocalSource()
+    }
+
+    private fun renderData(appState: AppState) {
+        when(appState) {
+            is AppState.Success -> {
+                val weatherData = appState.weatherData
+                binding.loadingLayout.visibility = View.GONE
+                setData(weatherData)
+            }
+
+            is AppState.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE
+            }
+
+            is AppState.Error -> {
+                binding.loadingLayout.visibility = View.GONE
+                Snackbar.make(requireView(), "Error", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Reload") { viewModel.getWeatherFromLocalSource() }
+                    .show()
+            }
+        }
+    }
+
+    private fun setData(weatherData: Weather) {
+        binding.apply {
+            cityName.text = weatherData.city.name
+            cityCoordinates.text = String.format(
+                getString(R.string.city_coordinates),
+                weatherData.city.lat.toString(),
+                weatherData.city.lon.toString()
+            )
+            temperatureValue.text = weatherData.temperature.toString()
+            feelsLikeValue.text = weatherData.feelsLike.toString()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
